7bf2ac8dbfa50fb78ec4e47801007a1edb750706
niall <niall@ngrimley15.lan>
2023-09-03 21:38:40 -0400

    first commit

---
+++ Node.java
@@ -0,0 +1,34 @@
+public class Node {
+	/**
+	 * @author njohnson3
+	 *
+	 */
+	class Node<T> {
+		T data;
+		Node<T> nextNode;
+		
+		
+		
+		public Node()
+		{
+			this.nextNode=null;
+		}
+		
+		
+		
+		public void setData(T d) {
+			this.data = d;
+		}
+		
+		public Node<T> getNode() {
+			return this.nextNode;
+		}
+		
+		public void setNextNode(Node<T> nextNode)
+		{
+			this.nextNode=nextNode;
+		}
+		
+		
+	}
+}
