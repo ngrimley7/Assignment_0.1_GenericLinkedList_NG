c816802100df4cdf76c161b3a629811736250a7b
niall <niall@10.10.28.197>
2023-09-05 20:15:19 -0400

    generic bag

---
+++ Genericbag/Application.java
@@ -0,0 +1,23 @@
+package Genericbag;
+
+public class Application {
+
+	public static void main(String[] args) 
+	{
+		Item<String> sb1 = new Item<String>("Saaki Vishnumolakala");
+		
+		Item<Integer> sb2 = new Item<Integer>(17);
+		
+		
+		
+		
+		SmallBag<Item> sb3 = new SmallBag<Item>();
+		
+		sb3.setSmallBag(sb1);
+		System.out.println(sb3.getSmallBag().getItem());
+		
+		sb3.setSmallBag(sb2);
+		System.out.println(sb3.getSmallBag().getItem());
+	}
+
+}
---
+++ Genericbag/Item.java
@@ -0,0 +1,21 @@
+package Genericbag;
+
+public class Item<T> 
+{
+	private T e;
+	
+	public Item(T t)
+	{
+		e = t;
+	}
+	
+	public void setItem(T e)
+	{
+		this.e = e;
+	}
+	
+	public T getItem()
+	{
+		return e;
+	}
+}
