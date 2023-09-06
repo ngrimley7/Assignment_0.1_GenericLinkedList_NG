20670f1e54549bde58fdf7f72a6d32361167aa5a
niall <niall@10.10.28.197>
2023-09-06 18:56:28 -0400

    commit

--- LinkedList.java
+++ LinkedList.java
@@ -1,78 +1,75 @@
 package linkedList;
+
 /**
- * @author njohnson3
+ * The LinkedList class represents a singly linked list of elements of generic type T.
  *
+ * @param <T> The type of elements stored in the linked list.
  */
 public class LinkedList<T> {
-	
-	Node<T> head;
-	int length;
-	Node<T> tail;
-	
-	
-	public LinkedList()
-	{
-		head=null;
-		this.length = 0;
-		tail=head;
-	}
-	
-	
-	public boolean isEmpty()
-	{
-		if (head==null) { 
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	
-	public Node<T> getList()
-	{
-		return head;
-	}
-	
-	
-	public void addNode(Node<T> aNode)
-	{
-		if (isEmpty() ) {
-			head = aNode;
-			tail=head;
-			return;
-		}
-		tail.nextNode=aNode;
-		tail=tail.nextNode;
-		tail.nextNode=null;
-		length++;
-		
-	}
-	
-	
-	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList<String> myList = new LinkedList<String>();
-		
-		Node<String> aNode = new Node<String>();
-		aNode.setData("Niall");
-		myList.addNode(aNode);
-		
-		aNode = new Node<String>();
-		aNode.setData("Grimley");
-		myList.addNode(aNode);
-		
-		Node<String> tempnode = myList.getList();
-		do 
-		{
-			System.out.println(tempnode.data);
-			tempnode=tempnode.nextNode;
-		} while (tempnode!=null);
-			
-		
-		
+    private Node<T> head;
+    private int length;
+    private Node<T> tail;
 
-	}
+    /**
+     * Constructs an empty linked list.
+     */
+    public LinkedList() {
+        head = null;
+        length = 0;
+        tail = head;
+    }
 
+    /**
+     * Checks if the linked list is empty.
+     *
+     * @return true if the linked list is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    /**
+     * Gets the head node of the linked list.
+     *
+     * @return The head node of the linked list.
+     */
+    public Node<T> getList() {
+        return head;
+    }
+
+    /**
+     * Adds a node to the end of the linked list.
+     *
+     * @param aNode The node to be added.
+     */
+    public void addNode(Node<T> aNode) {
+        if (isEmpty()) {
+            head = aNode;
+            tail = head;
+        } else {
+            tail.nextNode = aNode;
+            tail = tail.nextNode;
+            tail.nextNode = null;
+        }
+        length++;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> myList = new LinkedList<String>();
+
+        Node<String> aNode = new Node<String>();
+        aNode.setData("Niall");
+        myList.addNode(aNode);
+
+        aNode = new Node<String>();
+        aNode.setData("Grimley");
+        myList.addNode(aNode);
+
+        Node<String> tempNode = myList.getList();
+        do {
+            System.out.println(tempNode.data);
+            tempNode = tempNode.nextNode;
+        } while (tempNode != null);
+    }
 }
--- Node.java
+++ Node.java
@@ -1,34 +1,52 @@
-public class Node {
-	/**
-	 * @author njohnson3
-	 *
-	 */
-	class Node<T> {
-		T data;
-		Node<T> nextNode;
-		
-		
-		
-		public Node()
-		{
-			this.nextNode=null;
-		}
-		
-		
-		
-		public void setData(T d) {
-			this.data = d;
-		}
-		
-		public Node<T> getNode() {
-			return this.nextNode;
-		}
-		
-		public void setNextNode(Node<T> nextNode)
-		{
-			this.nextNode=nextNode;
-		}
-		
-		
-	}
+/**
+ * The Node class represents a single node in a linked list.
+ * Each node contains data of generic type T and a reference to the next node.
+ *
+ * @param <T> The type of data stored in the node.
+ */
+public class Node<T> {
+
+    /**
+     * Data stored in this node.
+     */
+    private T data;
+
+    /**
+     * Reference to the next node.
+     */
+    private Node<T> nextNode;
+
+    /**
+     * Constructs a new Node with no data and a null reference to the next node.
+     */
+    public Node() {
+        this.nextNode = null;
+    }
+
+    /**
+     * Sets the data of this node.
+     *
+     * @param d The data to be set in this node.
+     */
+    public void setData(T d) {
+        this.data = d;
+    }
+
+    /**
+     * Gets the next node in the linked list.
+     *
+     * @return The next node in the list.
+     */
+    public Node<T> getNextNode() {
+        return this.nextNode;
+    }
+
+    /**
+     * Sets the reference to the next node.
+     *
+     * @param nextNode The next node to be linked to this node.
+     */
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
 }
